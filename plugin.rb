2d19b598b68f938b39f5d645f4e6c8ecc0e5f436
Dustin Riley <rileymdustin@gmail.com>
2026-07-18 22:31:55 -0400

    feat: plugin scaffold and site settings

---
+++ plugin.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# name: discourse-redhawks-schedule
+# about: Fetches the Miami University Athletics RSS calendar and serves upcoming events as JSON.
+# version: 0.1.0
+# authors: MiamiHawkTalk
+# url: https://github.com/dustin-riley/discourse-redhawks-schedule
+
+enabled_site_setting :redhawks_schedule_enabled
+
+module ::RedhawksSchedule
+  PLUGIN_NAME = "discourse-redhawks-schedule"
+  STORE_KEY = "events"
+end
+
+require_relative "lib/redhawks_schedule/parser"
+
+after_initialize do
+  require_relative "app/jobs/scheduled/fetch_redhawks_schedule"
+  require_relative "app/controllers/redhawks_schedule_controller"
+
+  Discourse::Application.routes.append do
+    get "/redhawks-schedule" => "redhawks_schedule#index", :format => :json
+  end
+end
